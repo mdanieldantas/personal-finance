318e940a4cfbf5221c0d8012b82a396487bcae98
mdanieldantas <contatomarcosdgomes@gmail.com>
2024-12-18 17:36:40 -0300

    adiciona arquivo db.json com transações e ajusta dependências no package.json

--- index.js
+++ index.js
@@ -4,16 +4,15 @@ function createTransactionContainer(id) {
     container.classList.add('transaction')
     container.id = `transaction-${id}`
     return container
-  }
-  
-// criando o span de nome da transação class transaction-title e passando o nome da transação  como parametro  
-  function createTransactionTitle(name) {
+}
+
+// criando o span de nome da transação class transaction-title e passando o nome da transação como parametro  
+function createTransactionTitle(name) {
     const title = document.createElement('span')
     title.classList.add('transaction-title')
     title.textContent = name
     return title
-  }
-
+}
 
 // criando o span de data e o span de valor da transação com a classe transaction-amount 
 function createTransactionAmount(amount) {
@@ -22,52 +21,66 @@ function createTransactionAmount(amount) {
 
     // formatação do valor da transação para moeda brasileira com a biblioteca intl number format 
     const formater = Intl.NumberFormat('pt-BR', {
-      compactDisplay: 'long',
-      currency: 'BRL',
-      style: 'currency',
+        compactDisplay: 'long',
+        currency: 'BRL',
+        style: 'currency',
     })
 
     // verificando se o valor da transação e positivo ou negativo e adicionando a classe credit ou debit
     const formatedAmount = formater.format(amount)
     if (amount > 0) {
-      span.textContent = `${formatedAmount} C`
-      span.classList.add('credit')
+        span.textContent = `${formatedAmount} C`
+        span.classList.add('credit')
     } else {
         // verificando se o valor da transação e negativo e adicionando a classe credit ou debit
-      span.textContent = `${formatedAmount} D`
-      span.classList.add('debit')
+        span.textContent = `${formatedAmount} D`
+        span.classList.add('debit')
     }
     // retornando o span com o valor da transação formatado e a classe credit ou debit 
     return span
-  }
-
-  // funcao assincrona para buscar as transações do backend
-  async function fetchTransactions() {
-    return await fetch('http://localhost:3000/transactions').then(res => res.json())
-  }
-
-
-  // array para armazenar as transações  
-  let transactions = []
-
-  // funcao assincrona para buscar as transações do backend e atualizar o saldo
-function updateBalance() {
-    // balanço da conta com base nas transações do backend 
-  const balanceSpan = document.querySelector('#balance')
-    // somando o valor das transações 
-  const balance = transactions.reduce((sum, transaction) => sum + transaction.amount, 0)
-    // formatação do valor do balanço para moeda brasileira com a biblioteca intl number format
-  const formater = Intl.NumberFormat('pt-BR', {
-    compactDisplay: 'long',
-    currency: 'BRL',
-    style: 'currency'
-  })
-    // adicionando o valor do balanço na tela
-  balanceSpan.textContent = formater.format(balance)
 }
 
+// função para renderizar uma transação na tela
+function renderTransaction(transaction) {
+    const container = createTransactionContainer(transaction.id)
+    const title = createTransactionTitle(transaction.name)
+    const amount = createTransactionAmount(transaction.amount)
+    const editBtn = createEditTransactionBtn(transaction)
+    const deleteBtn = createDeleteTransactionButton(transaction.id)
 
-// funcao setup para buscar as transações do backend e atualizar o saldo
+    container.appendChild(title)
+    container.appendChild(amount)
+    container.appendChild(editBtn)
+    container.appendChild(deleteBtn)
+
+    document.querySelector('main').appendChild(container)
+}
+
+// função assíncrona para buscar as transações do backend
+async function fetchTransactions() {
+    return await fetch('http://localhost:3000/transactions').then(res => res.json())
+}
+
+// array para armazenar as transações  
+let transactions = []
+
+// função assíncrona para buscar as transações do backend e atualizar o saldo
+function updateBalance() {
+    // balanço da conta com base nas transações do backend 
+    const balanceSpan = document.querySelector('#balance')
+    // somando o valor das transações 
+    const balance = transactions.reduce((sum, transaction) => sum + transaction.amount, 0)
+    // formatação do valor do balanço para moeda brasileira com a biblioteca intl number format
+    const formater = Intl.NumberFormat('pt-BR', {
+        compactDisplay: 'long',
+        currency: 'BRL',
+        style: 'currency'
+    })
+    // adicionando o valor do balanço na tela
+    balanceSpan.textContent = formater.format(balance)
+}
+
+// função setup para buscar as transações do backend e atualizar o saldo
 async function setup() {
     // buscando as transações do backend
     const results = await fetchTransactions()
@@ -77,151 +90,80 @@ async function setup() {
     transactions.forEach(renderTransaction)
     // atualizando o saldo da conta     
     updateBalance()
-  }
-  
-    // funcao para renderizar as transações na tela quando o DOM for carregado 
-  document.addEventListener('DOMContentLoaded', setup)
+}
 
+// função para renderizar as transações na tela quando o DOM for carregado 
+document.addEventListener('DOMContentLoaded', setup)
 
-  // ...
-// funcao para salvar a transação no backend
+// função para salvar a transação no backend
 async function saveTransaction(ev) {
-    // previnindo o comportamento padrão do formulário de enviar o formulário  e recarregar a página limpando o formulário 
+    // previnindo o comportamento padrão do formulário de enviar o formulário e recarregar a página limpando o formulário 
     ev.preventDefault()
-  // pegando os dados do formulário
-    const name = document.querySelector('#name').value
-    // verificando se o valor da transação e negativo e adicionando a classe credit ou debit
-    const amount = parseFloat(document.querySelector('#amount').value)
-  // enviando os dados do formulário para o backend
-    const response = await fetch('http://localhost:3000/transactions', {
-      method: 'POST',
-      body: JSON.stringify({ name, amount }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-    // pegando a resposta do backend
-    const transaction = await response.json()
-    // adicionando a transação ao array
-    transactions.push(transaction)
-    // renderizando a transação na tela
-    renderTransaction(transaction)
-    // limpando o formulário com os dados da transação usando o reset do formulário 
-    ev.target.reset()
-    // atualizando o saldo da conta com a funcao updateBalance 
-    updateBalance()
-  }
-  
-  // ...
-    // funcao para renderizar as transações na tela quando o DOM for carregado
-  document.addEventListener('DOMContentLoaded', setup)
-  
-  // funcao para salvar a transação no backend  quando o formulário for submetido
-  document.querySelector('form').addEventListener('submit', saveTransaction)
-
-
-  // ...
-// funcao para editar a transação no backend com botao de editar 
-function createEditTransactionBtn(transaction) {
-    // criando o botão de editar com a classe edit-btn e o texto editar
-    const editBtn = document.createElement('button')
-    // adicionando a classe edit-btn ao botão
-    editBtn.classList.add('edit-btn')
-    // adicionando o texto editar ao botão
-    editBtn.textContent = 'Editar'
-    // adicionando o evento de click ao botão para pegar os dados da transação e preencher o formulário
-    editBtn.addEventListener('click', () => {
-      // preenchendo o formulário com os dados da transação
-      document.querySelector('#id').value = transaction.id
-      // verificando se o valor da transação e negativo e adicionando a classe credit ou debit
-      document.querySelector('#name').value = transaction.name
-        // verificando se o valor da transação e negativo e adicionando a classe credit ou debit
-      document.querySelector('#amount').value = transaction.amount
-    })
-    // retornando o botão de editar
-    return editBtn
-  }
-  
-
-// funcao para salvar a transação no backend  quando o formulário for submetido
-async function saveTransaction(ev) {
-    // previnindo o comportamento padrão do formulário de enviar o formulário  e recarregar a página limpando o formulário
-    ev.preventDefault()
-  
     // pegando os dados do formulário
     const id = document.querySelector('#id').value
-    // verificando se o valor da transação e negativo e adicionando a classe credit ou debit
     const name = document.querySelector('#name').value
-    // verificando se o valor da transação e negativo e adicionando a classe credit ou debit
     const amount = parseFloat(document.querySelector('#amount').value)
-  
+
     // enviando os dados do formulário para o backend para salvar a transação
     if (id) {
-      const response = await fetch(`http://localhost:3000/transactions/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({ name, amount }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-        // pegando a resposta do backend
-      const transaction = await response.json()
-        // encontrando a transação no array de transações e removendo a transação antiga e adicionando a nova transação
-      const indexToRemove = transactions.findIndex((t) => t.id === id)
-        // encontrando a transação no array de transações e removendo a transação antiga e adicionando a nova transação
-      transactions.splice(indexToRemove, 1, transaction)
-        // encontrando a transação na tela e removendo a transação antiga e adicionando a nova transação
-      document.querySelector(`#transaction-${id}`).remove()
-        // encontrando a transação na tela e removendo a transação antiga e adicionando a nova transação
-      renderTransaction(transaction)
+        const response = await fetch(`http://localhost:3000/transactions/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ name, amount }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const transaction = await response.json()
+        const indexToRemove = transactions.findIndex((t) => t.id === id)
+        transactions.splice(indexToRemove, 1, transaction)
+        document.querySelector(`#transaction-${id}`).remove()
+        renderTransaction(transaction)
     } else {
-      const response = await fetch('http://localhost:3000/transactions', {
-        method: 'POST',
-        body: JSON.stringify({ name, amount }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      // pegando a resposta do backend
-      const transaction = await response.json()
-      // adicionando a transação ao array
-      transactions.push(transaction)
-      // renderizando a transação na tela
-      renderTransaction(transaction)
+        const response = await fetch('http://localhost:3000/transactions', {
+            method: 'POST',
+            body: JSON.stringify({ name, amount }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const transaction = await response.json()
+        transactions.push(transaction)
+        renderTransaction(transaction)
     }
-  
+
     // limpando o formulário
     ev.target.reset()
-  
     // atualizando o saldo da conta
     updateBalance()
-  }
-  
-  // ...
+}
 
-  // ...
-// funcao para excluir a transação no backend com botao de excluir
-function createDeleteTransactionButton(id) {
-    // criando o botão de excluir com a classe delete-btn e o texto excluir
-    const deleteBtn = document.createElement('button')
-    // adicionando a classe delete-btn ao botão
-    deleteBtn.classList.add('delete-btn')
-    // adicionando o texto excluir ao botão
-    deleteBtn.textContent = 'Excluir'
-    // adicionando o evento de click ao botão para excluir a transação
-    deleteBtn.addEventListener('click', async () => {
-        // enviando a requisição para excluir a transação no backend
-      await fetch(`http://localhost:3000/transactions/${id}`, { method: 'DELETE' })
-      deleteBtn.parentElement.remove()
-        // encontrando a transação no array de transações e removendo a transação
-      const indexToRemove = transactions.findIndex((t) => t.id === id)
-        // encontrando a transação no array de transações
-      transactions.splice(indexToRemove, 1)
-        // atualizando o saldo da conta
-      updateBalance()
+// função para editar a transação no backend com botão de editar 
+function createEditTransactionBtn(transaction) {
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit-btn')
+    editBtn.textContent = 'Editar'
+    editBtn.addEventListener('click', () => {
+        document.querySelector('#id').value = transaction.id
+        document.querySelector('#name').value = transaction.name
+        document.querySelector('#amount').value = transaction.amount
     })
-    // retornando o botão de excluir
+    return editBtn
+}
+
+// função para excluir a transação no backend com botão de excluir
+function createDeleteTransactionButton(id) {
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete-btn')
+    deleteBtn.textContent = 'Excluir'
+    deleteBtn.addEventListener('click', async () => {
+        await fetch(`http://localhost:3000/transactions/${id}`, { method: 'DELETE' })
+        deleteBtn.parentElement.remove()
+        const indexToRemove = transactions.findIndex((t) => t.id === id)
+        transactions.splice(indexToRemove, 1)
+        updateBalance()
+    })
     return deleteBtn
-  }
-  
-  
+}
+
+// função para salvar a transação no backend quando o formulário for submetido
+document.querySelector('form').addEventListener('submit', saveTransaction)
